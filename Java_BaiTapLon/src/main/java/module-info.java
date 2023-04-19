f4ad1e8341faa59bbae78c612d349c60fabcf05d
Khimisreal <gobisteel.152@gmail.com>
2023-04-19 19:19:35 +0700

    'update'

--- Java_BaiTapLon/src/main/java/module-info.java
+++ Java_BaiTapLon/src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module com.baokhiem.java_baitaplon {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
+    requires java.base;
 
     opens com.baokhiem.java_baitaplon to javafx.fxml;
     exports com.baokhiem.java_baitaplon;
